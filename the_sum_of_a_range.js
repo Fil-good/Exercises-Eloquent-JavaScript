ed5dd06ccb8c2001f1cbae82ac94e866933f2e8e
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-21 12:15:38 +0100

    sum of array first notation of function

--- the_sum_of_a_range.js
+++ the_sum_of_a_range.js
@@ -12,19 +12,18 @@ function range(start_value, end_value) {
   console.log(`yo! ${array_of_numbers}`);
 }
 
-console.log("test");
-console.log(range(0,5));
+// console.log("test");
+// console.log(range(0, 5));
 
 
+function sum(array) {
+  sum_of_values = 0;
+  array.forEach(element => {
+  sum_of_values += element;
 
+});
+console.log(sum_of_values);
+}
 
-// browser try-out
-
-// const range = function(start_value, end_value) {
-//   let array_of_numbers = [];
-//   for (let number = start_value; number < end_value; number = number + 1) {
-//     array_of_numbers.push(number)
-//   }
-//   console.log(array_of_numbers);
-// }
-// range(1, 5);
+array_to_sum = [1,2,3,4,5];
+sum(array_to_sum);
