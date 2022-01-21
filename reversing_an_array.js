e5457dca4033dcfdad5141c8f87c55c0234a3801
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-21 14:48:35 +0100

    reversing an array with temp_array

--- reversing_an_array.js
+++ reversing_an_array.js
@@ -4,23 +4,29 @@
 
 function reverseArray(array) {
   new_array =[];
-  for(let index = 0; index < array.length; index += 1)
+  for(let index = 1; index < array.length + 1; index += 1) {
   new_array.push(array[array.length - index]);
-
+  }
   console.log(new_array);
-
-
 }
 
 // example
 given_array = [1,2,3,4,5];
-reverseArray(given_array);
+// reverseArray(given_array);
 reverseArrayInPlace(given_array);
 
 
 // modifies the array
 function reverseArrayInPlace(array) {
-  for(let index = 0; index < array.length; index += 1)
-  array = array.push(array[array.length - index]);
-
-}
+  i = 0;
+  temp_array = [];
+  for(let index = given_array.length - 1 ; index > -1 ; index -= 1) {
+    temp_array[i] = array[index];
+    console.log(`temp_array[${i}] = array[${index}]`);
+    i +=1;
+    console.log(`array array iterative values after change: ${array}`);
+  }
+  console.log(array);
+  array = temp_array;
+  console.log(array);
+} /* [ 5, 4, 3, 4, 5 ] */
