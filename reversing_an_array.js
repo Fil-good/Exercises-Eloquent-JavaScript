570d8e0959e1b8ae74ab3d98dde8dfca7ce15102
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-21 13:24:43 +0100

    reverseArrayInPlace

--- reversing_an_array.js
+++ reversing_an_array.js
@@ -15,10 +15,12 @@ function reverseArray(array) {
 // example
 given_array = [1,2,3,4,5];
 reverseArray(given_array);
+reverseArrayInPlace(given_array);
 
 
 // modifies the array
 function reverseArrayInPlace(array) {
-
+  for(let index = 0; index < array.length; index += 1)
+  array = array.push(array[array.length - index]);
 
 }
