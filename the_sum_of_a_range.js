01d68ccba83b3f8bbcb67cbfa225a5c983026682
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-21 13:06:09 +0100

    optional sum with stepj

--- the_sum_of_a_range.js
+++ the_sum_of_a_range.js
@@ -26,15 +26,16 @@ console.log(sum_of_values);
 }
 
 array_to_sum = [1,2,3,4,5];
-sum(array_to_sum);
+// sum(array_to_sum);
+sum2(array_to_sum,2);
 
-function sum2(array) {
-  sum_of_values = 0;
-  array.forEach((element) => {
-    sum_of_values += element;
-  })
-  console.log(sum_of_values);
-}
 
-array_to_sum2 = [1,2,3,4,5,6];
-sum(array_to_sum2);
+// sum of array, with extra argument that indicates the "step"
+function sum2(array, step) {
+  adjusted_array = [];
+  for(let counter = 0; counter < array.length; counter += step) {
+    adjusted_array.push(array[counter]);
+  }
+  sum(adjusted_array);
+  console.log(adjusted_array);
+  };
