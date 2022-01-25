a100c36a1171013c603488b7642eca53b83549ec
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-25 15:09:45 +0100

    elegant style

--- deep_comparison.js
+++ deep_comparison.js
@@ -1,17 +1,29 @@
-//
 
-function deepEqual(value1, value2) {
-  if (value1 === value2) {
+function deepEqual(a, b) {
+  if (a === b) {
+    // items are identical
     return true;
-  }
-    else {
-      return false;
+  } else if (typeof a === 'object' && a !== null && typeof b === 'object' && b !== null) {
+    // items are objects - do a deep property value compare
+    // join keys from both objects together in one array
+    let keys = Object.keys(a).concat(Object.keys(b));
+    // filter out duplicate keys
+    keys = keys.filter(
+      function (value, index, self) {
+        return self.indexOf(value) === index;
+      }
+    );
+    for (p of keys) {
+      if (typeof a[p] === 'object' && typeof b[p] === 'object') {
+        if (deepEqual(a[p], b[p]) === false) {
+          return false;
+        }
+      } else if (a[p] !== b[p]) {
+        return false;
+      }
     }
-
+    return true;
+  } else {
+    return false;
+  }
 }
-
-let value1 = 5;
-let value2 = 6;
-let value3 = 5;
-console.log(`deepEqual: ${deepEqual(value1, value3)}`);
-console.log(`deepEqual: ${deepEqual(value1, value2)}`);
