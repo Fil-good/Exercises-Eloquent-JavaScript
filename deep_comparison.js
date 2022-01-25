15a03c8ac0c971b0b8318c9249175ab738bc8673
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-25 13:42:44 +0100

    deep comparison start

---
+++ deep_comparison.js
@@ -0,0 +1,12 @@
+//
+
+function deepEqual(value1, value2) {
+  if value1 === value2 {
+    return true;
+  }
+}
+
+let value1 = 5;
+let value2 = 6;
+let value3 = 5;
+console.log(`deepEqual: ${deepEqual(value1, value3)}`);
