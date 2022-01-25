acd5d8c7b0b2c667a10eb9ac82a1bfecdfb87700
Filip Brouwers <filip.brouwers@gmail.com>
2022-01-25 13:48:13 +0100

    deep comparison values ok

--- deep_comparison.js
+++ deep_comparison.js
@@ -4,9 +4,14 @@ function deepEqual(value1, value2) {
   if (value1 === value2) {
     return true;
   }
+    else {
+      return false;
+    }
+
 }
 
 let value1 = 5;
 let value2 = 6;
 let value3 = 5;
 console.log(`deepEqual: ${deepEqual(value1, value3)}`);
+console.log(`deepEqual: ${deepEqual(value1, value2)}`);
